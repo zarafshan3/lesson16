f856c3bda777aa614495f5bb0269c9b7c4f497e6
zarafshan3 <ejazzarafshan@gmail.com>
2024-07-17 18:01:30 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function chbgcolor(color){
+    document.body.style.background = color
+}
+
+function student(firstname, lastname, age = 17, Status ="Active"){
+    console.log(`my name is ${firstname} ${lastname}. I am ${age} years old. My study status is ${Status}  `)
+}
+
+
+
+
+
